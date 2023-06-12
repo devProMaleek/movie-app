946cdcf9bddfaba619dc8e1b94838ca547caee89
Abdulmalik Adebayo <milikiadbay@gmail.com>
2023-06-12 15:27:52 +0100

    feat: Add the movies screen component

---
+++ screens/MovieScreen.tsx
@@ -0,0 +1,14 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+type Props = {}
+
+const MovieScreen = (props: Props) => {
+  return (
+    <View>
+      <Text>MovieScreen</Text>
+    </View>
+  )
+}
+
+export default MovieScreen

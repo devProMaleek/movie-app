95b82e5d7558f7d012cf7b5b50a4938fbdefd037
Abdulmalik Adebayo <milikiadbay@gmail.com>
2023-06-12 15:29:00 +0100

    feat: Add navigation for movie screen

--- Navigation.tsx
+++ Navigation.tsx
@@ -1,6 +1,7 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from './screens/HomeScreen';
+import MovieScreen from './screens/MovieScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -15,6 +16,7 @@ const Navigation = (props: Props) => {
         }}
       >
         <Stack.Screen name="Home" options={{ headerShown: false }} component={HomeScreen} />
+        <Stack.Screen name="Movie" options={{ headerShown: false }} component={MovieScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );

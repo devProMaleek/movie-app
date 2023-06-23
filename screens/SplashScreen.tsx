afdbcbd66d3382a8fbd334afaf4360c3140e5355
Abdulmalik Adebayo <milikiadbay@gmail.com>
2023-06-23 23:30:26 +0100

    refactor: Change the UI of the splash scren

--- screens/SplashScreen.tsx
+++ screens/SplashScreen.tsx
@@ -1,18 +1,17 @@
-import React, { useEffect } from 'react'
-import { Text, View } from 'react-native'
+import React, { useEffect } from 'react';
+import { Text, View, Platform } from 'react-native';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import { RouteProp } from '@react-navigation/native';
 import { styles } from '../themes';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { StatusBar } from 'expo-status-bar';
 
 type RootStackParamList = {
   SplashScreen: undefined;
   HomeScreen: undefined;
 };
 
-type SplashScreenNavigationProp = NativeStackNavigationProp<
-  RootStackParamList,
-  'SplashScreen'
->;
+type SplashScreenNavigationProp = NativeStackNavigationProp<RootStackParamList, 'SplashScreen'>;
 
 type SplashScreenRouteProp = RouteProp<RootStackParamList, 'SplashScreen'>;
 
@@ -20,6 +19,8 @@ type SplashScreenProps = {
   navigation: SplashScreenNavigationProp;
   route: SplashScreenRouteProp;
 };
+
+const isIOS = Platform.OS === 'ios';
 
 const SplashScreen = ({ navigation }: SplashScreenProps) => {
   useEffect(() => {
@@ -30,12 +31,15 @@ const SplashScreen = ({ navigation }: SplashScreenProps) => {
     return () => clearTimeout(timeout);
   }, [navigation]);
   return (
-    <View className='flex flex-1 justify-center items-center bg-neutral-800'>
-      <Text className="text-white text-4xl font-bold">
-            <Text style={styles.text}>M</Text>ovie<Text style={styles.text}>X</Text>pertise
-          </Text>
+    <View className="flex flex-1 justify-center items-center bg-neutral-800">
+      <SafeAreaView className={`${isIOS ? '-mb-2' : 'mb-3'} `}>
+        <StatusBar style="light" />
+        <Text className="text-white text-4xl font-bold">
+          <Text style={styles.text}>M</Text>ovie<Text style={styles.text}>X</Text>pertise
+        </Text>
+      </SafeAreaView>
     </View>
-  )
-}
+  );
+};
 
-export default SplashScreen
+export default SplashScreen;

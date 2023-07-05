2b1fafdd9665b2abb6129b59f3517213e9516a24
Abdulmalik Adebayo <milikiadbay@gmail.com>
2023-07-06 00:13:13 +0100

    feat: Add the Cast screen

---
+++ screens/CastScreen.tsx
@@ -0,0 +1,14 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+
+type Props = {};
+
+const CastScreen = (props: Props) => {
+  return (
+    <View>
+      <Text>CastScreen</Text>
+    </View>
+  );
+};
+
+export default CastScreen;

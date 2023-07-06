fb816dd5f7a29ac30b13e876cee7f684423878b7
Abdulmalik Adebayo <milikiadbay@gmail.com>
2023-07-06 16:52:20 +0100

    feat: Add Screench screen to the navigator

--- Navigation.tsx
+++ Navigation.tsx
@@ -4,12 +4,14 @@ import HomeScreen from './screens/HomeScreen';
 import MovieScreen from './screens/MovieScreen';
 import SplashScreen from './screens/SplashScreen';
 import CastScreen from './screens/CastScreen';
+import SearchScreen from './screens/SearchScreen';
 
 type RootStackParamList = {
   SplashScreen: undefined;
   HomeScreen: undefined;
   MovieScreen: undefined;
   CastScreen: undefined;
+  SearchScreen: undefined; 
 };
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
@@ -29,6 +31,7 @@ const Navigation = (props: Props) => {
         <Stack.Screen name="HomeScreen" options={{ headerShown: false }} component={HomeScreen} />
         <Stack.Screen name="MovieScreen" options={{ headerShown: false }} component={MovieScreen} />
         <Stack.Screen name="CastScreen" options={{ headerShown: false }} component={CastScreen} />
+        <Stack.Screen name="SearchScreen" options={{ headerShown: false }} component={SearchScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );

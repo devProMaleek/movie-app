2ac61f2f164bdbd7f34107b4e2b5cd89b3b332b0
Abdulmalik Adebayo <milikiadbay@gmail.com>
2023-07-17 23:27:45 +0100

    refactor: Finish building the search screen.

--- screens/SearchScreen.tsx
+++ screens/SearchScreen.tsx
@@ -1,11 +1,22 @@
-import { View, Text, Dimensions, TextInput, TouchableOpacity, Button } from 'react-native';
-import React, { useCallback } from 'react';
+import {
+  View,
+  Text,
+  Dimensions,
+  TextInput,
+  TouchableOpacity,
+  Button,
+  ScrollView,
+  TouchableWithoutFeedback,
+  Image,
+} from 'react-native';
+import React, { useCallback, useState } from 'react';
 import { useNavigation, useRoute, ParamListBase } from '@react-navigation/native';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { useForm, Controller, SubmitHandler } from 'react-hook-form';
 import { XMarkIcon } from 'react-native-heroicons/outline';
 import { theme } from '../themes';
+import Loading from '../components/Loading';
 
 type Props = {};
 type FormValues = {
@@ -15,6 +26,9 @@ type FormValues = {
 const { width, height } = Dimensions.get('window');
 
 const SearchScreen = (props: Props) => {
+  const [searchResults, setSearchResults] = useState<number[]>([1, 2, 3, 4, 5, 6, 7, 8]);
+  const [loading, setLoading] = useState<boolean>(false);
+  let movieName: string = 'Ant-Man and the Wasp: Quantumania';
   const {
     control,
     handleSubmit,
@@ -34,33 +48,75 @@ const SearchScreen = (props: Props) => {
 
   return (
     <SafeAreaView className="bg-neutral-800 flex-1">
-      <View className="flex-row justify-between items-center">
-        <View className="mb-3 w- flex-row justify-between items-center border border-neutral-500 rounded-full">
-          <Controller
-            control={control}
-            render={({ field: { onChange, onBlur, value } }) => (
-              <TextInput
-                onChangeText={onChange}
-                onBlur={onBlur}
-                value={value}
-                placeholder="Search Movies"
-                placeholderTextColor={'lightgray'}
-                className="py-1 pl-6 flex-1 text-base font-semibold text-white tracking-wider"
-              />
-            )}
-            name="searchValue"
-            rules={{ required: 'Search Value is required' }}
-            defaultValue=""
-          />
+      <View className="mb-3 mx-4 flex-row justify-between items-center border border-neutral-500 rounded-full">
+        <Controller
+          control={control}
+          render={({ field: { onChange, onBlur, value } }) => (
+            <TextInput
+              onChangeText={onChange}
+              onBlur={onBlur}
+              value={value}
+              placeholder="Search Movies"
+              placeholderTextColor={'lightgray'}
+              className="py-1 pl-6 flex-1 text-base font-semibold text-white tracking-wider"
+            />
+          )}
+          name="searchValue"
+          rules={{ required: 'Search Value is required' }}
+          defaultValue=""
+        />
 
-          <TouchableOpacity onPress={handleCancelPress} className="rounded-full p-3 m-1 bg-neutral-500">
-            <XMarkIcon size="25" color="white" />
-          </TouchableOpacity>
-        </View>
         <TouchableOpacity onPress={handleCancelPress} className="rounded-full p-3 m-1 bg-neutral-500">
-            <XMarkIcon size="25" color="white" />
-          </TouchableOpacity>
+          <XMarkIcon size="25" color="white" />
+        </TouchableOpacity>
       </View>
+
+      {loading ? (
+        <>
+          <Loading />
+        </>
+      ) : (
+        <>
+          {searchResults.length > 0 ? (
+            <>
+              <ScrollView
+                showsVerticalScrollIndicator={false}
+                contentContainerStyle={{ paddingHorizontal: 15 }}
+                className="space-y-3"
+              >
+                <Text className="text-white font-semibold ml-1 text-base">Results ({searchResults.length})</Text>
+                <View className="flex-row justify-between flex-wrap">
+                  {searchResults.map((result, index) => {
+                    return (
+                      <TouchableWithoutFeedback
+                        key={index}
+                        onPress={() => navigation.push('MovieScreen', result as Object)}
+                      >
+                        <View className="space-y-2 mb-4">
+                          <Image
+                            className="rounded-3xl"
+                            source={require('../assets/images/moviePoster2.png')}
+                            style={{ width: width * 0.44, height: height * 0.3 }}
+                          />
+                          <Text className="text-neutral-300 ml-1">
+                            {movieName.length > 14 ? `${movieName.slice(0, 14)}...` : movieName}
+                          </Text>
+                        </View>
+                      </TouchableWithoutFeedback>
+                    );
+                  })}
+                </View>
+              </ScrollView>
+            </>
+          ) : (
+            <>
+              <View className="flex-row justify-center">
+                <Image source={require('../assets/images/movieTime.png')} className="h-96 w-96" />
+              </View>
+            </>
+          )}
+        </>
+      )}
     </SafeAreaView>
   );
 };

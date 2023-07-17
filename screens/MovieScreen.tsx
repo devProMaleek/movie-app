c05e4dd00284f0bce483f2dbf0e69514c7731ae9
Abdulmalik Adebayo <milikiadbay@gmail.com>
2023-07-17 23:27:11 +0100

    refactor: Add the loading state to this screen.
    Add the loading component to show the loading state.

--- screens/MovieScreen.tsx
+++ screens/MovieScreen.tsx
@@ -9,6 +9,7 @@ import { HeartIcon } from 'react-native-heroicons/solid';
 import { LinearGradient } from 'expo-linear-gradient';
 import Cast from '../components/Cast';
 import MovieList from '../components/MovieList';
+import Loading from '../components/Loading';
 
 type Props = {};
 
@@ -23,6 +24,7 @@ const MovieScreen = (props: Props) => {
   const [isFavorite, setIsFavorite] = useState<boolean>(false);
   const [casts, setCasts] = useState<number[]>([1, 2, 3, 4, 5, 6, 7]);
   const [similarMovies, setSimilarMovies] = useState<number[]>([1, 2, 3, 4, 5, 6, 7]);
+  const [loading, setLoading] = useState<boolean>(false);
 
   const toggleFavorite = useCallback(() => {
     setIsFavorite(!isFavorite);
@@ -44,16 +46,24 @@ const MovieScreen = (props: Props) => {
             <HeartIcon size="35" color={isFavorite ? theme.background : 'white'} />
           </TouchableOpacity>
         </SafeAreaView>
-        <View>
-          <Image source={require('../assets/images/moviePoster2.png')} style={{ width, height: height * 0.55 }} />
-          <LinearGradient
-            colors={['transparent', 'rgba(23,23,23,0.8)', 'rgba(23,23,23,1)']}
-            style={{ width, height: height * 0.4 }}
-            start={{ x: 0.5, y: 0 }}
-            end={{ x: 0.5, y: 1 }}
-            className="absolute bottom-0"
-          />
-        </View>
+        {loading ? (
+          <>
+            <Loading />
+          </>
+        ) : (
+          <>
+            <View>
+              <Image source={require('../assets/images/moviePoster2.png')} style={{ width, height: height * 0.55 }} />
+              <LinearGradient
+                colors={['transparent', 'rgba(23,23,23,0.8)', 'rgba(23,23,23,1)']}
+                style={{ width, height: height * 0.4 }}
+                start={{ x: 0.5, y: 0 }}
+                end={{ x: 0.5, y: 1 }}
+                className="absolute bottom-0"
+              />
+            </View>
+          </>
+        )}
       </View>
 
       <View style={{ marginTop: -(height * 0.09) }} className="space-y-3">

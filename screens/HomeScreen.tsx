e0695ee621190872fa2203c448c825208133c9a7
Abdulmalik Adebayo <milikiadbay@gmail.com>
2023-07-18 03:53:31 +0100

    Feat: Update the home screen
    Fetch all the data for this screen

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { Text, View, Platform, TouchableOpacity, ScrollView } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { useNavigation, useRoute, ParamListBase } from '@react-navigation/native';
@@ -9,18 +9,59 @@ import { styles } from '../themes';
 import TrendingMovies from '../components/TrendingMovies';
 import MovieList from '../components/MovieList';
 import Loading from '../components/Loading';
+import { fetchTopRatedMovies, fetchTrendingMovies, fetchUpcomingMovies } from '../api/moviedb';
 
 type Props = {};
 
 const isIOS = Platform.OS === 'ios';
 
 const HomeScreen = (props: Props) => {
-  const [trendingMovies, setTrendingMovies] = useState([1, 2, 3, 4, 5, 6]);
-  const [upcomingMovies, setUpcomingMovies] = useState([1, 2, 3, 4, 5, 6]);
-  const [topRatedMovies, setTopRatedMovies] = useState([1, 2, 3, 4, 5, 6]);
+  const [trendingMovies, setTrendingMovies] = useState<Movie[]>();
+  const [upcomingMovies, setUpcomingMovies] = useState<Movie[]>();
+  const [topRatedMovies, setTopRatedMovies] = useState<Movie[]>();
   const [loading, setLoading] = useState<boolean>(false);
 
   const navigation = useNavigation<NativeStackNavigationProp<ParamListBase>>();
+
+  useEffect(() => {
+    setLoading(true);
+    const timeout = setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+
+    return () => clearTimeout(timeout);
+  }, []);
+
+  const getTrendingMovies = useCallback(async () => {
+    const data = await fetchTrendingMovies();
+    if (data && data.results) {
+      setTrendingMovies(data.results);
+    }
+    setLoading(false);
+  }, []);
+
+  const getUpcomingMovies = useCallback(async () => {
+    const data = await fetchUpcomingMovies();
+    if (data && data.results) {
+      setUpcomingMovies(data.results);
+    }
+    setLoading(false);
+  }, []);
+
+  const getTopRatedMovies = useCallback(async () => {
+    const data = await fetchTopRatedMovies();
+    if (data && data.results) {
+      setTopRatedMovies(data.results);
+    }
+    setLoading(false);
+  }, []);
+
+  useEffect(() => {
+    getTrendingMovies();
+    getUpcomingMovies();
+    getTopRatedMovies();
+  }, []);
+
   return (
     <View className="flex-1 bg-neutral-800">
       <SafeAreaView className={`${isIOS ? '-mb-2' : 'mb-3'} `}>
@@ -43,11 +84,11 @@ const HomeScreen = (props: Props) => {
       ) : (
         <>
           <ScrollView showsVerticalScrollIndicator={false} contentContainerStyle={{ paddingBottom: 10 }}>
-            <TrendingMovies data={trendingMovies} />
+            {trendingMovies && <TrendingMovies data={trendingMovies} />}
 
-            <MovieList title="Upcoming Movies" data={upcomingMovies} />
+            {upcomingMovies && <MovieList title="Upcoming Movies" data={upcomingMovies} />}
 
-            <MovieList title="Top Rated Movies" data={upcomingMovies} />
+            {topRatedMovies && <MovieList title="Top Rated Movies" data={topRatedMovies} />}
           </ScrollView>
         </>
       )}
